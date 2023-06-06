03a11be296fce7fbbc5d3966e4cd72b678a8f4f0
Mihkel Hain <Mihkelhain7@gmail.com>
2023-06-06 13:39:01 +0300

    added main Page Base

--- Part 1 - Displaying Data/MonkeyFinder/Model/Monkey.cs
+++ Part 1 - Displaying Data/MonkeyFinder/Model/Monkey.cs
@@ -5,8 +5,6 @@ namespace MonkeyFinder.Model;
 public class Monkey
 {
 
-    public class Root
-    {
         public string Name { get; set; }
         public string Location { get; set; }
         public string Details { get; set; }
@@ -14,5 +12,5 @@ public class Monkey
         public int Population { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-    }
+    
 }
